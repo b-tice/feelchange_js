ea111ed1b48ac456ba6ebb24255e09210b62b16c
b-tice <tice@alum.mit.edu>
2024-01-29 22:04:16 -0800

    update files for glitch

--- host_static_file/index.js
+++ host_static_file/index.js
@@ -3,7 +3,23 @@ const express = require('express');
 const app = express();
 
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Starting server at ${port}`);
+});
 
 // Serve up a static web page
 app.use(express.static('public'));
+
+app.use(express.json({ limit: "1mb" }));
+
+app.post("/api", (request, response) => {
+  console.log("I got a request!");
+  console.log(request.body);
+  const data = request.body;
+  response.json({
+    status: "success",
+    latitude: data.lat,
+    longitude: data.lon,
+  });
+});

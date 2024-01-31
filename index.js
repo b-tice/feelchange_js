bf5806dd94fb070ee063a6169d366ccb97551f34
b-tice <tice@alum.mit.edu>
2024-01-31 00:13:26 -0800

    max connection working

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-
+const Datastore = require('nedb');
+const fs = require('fs');
+const content = '1';
 const app = express();
 
 
@@ -13,10 +15,26 @@ app.use(express.static('public'));
 
 app.use(express.json({ limit: "1mb" }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+
 app.post("/api", (request, response) => {
   console.log("I got a request!");
   console.log(request.body);
   const data = request.body;
+  const timestamp = Date.now();
+  
+  fs.writeFile('public/test.txt', content, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('File Written OK');// file written successfully
+  }
+});
+  
+  data.timestamp = timestamp;
+  database.insert(data);
   response.json({
     status: "success",
     latitude: data.lat,

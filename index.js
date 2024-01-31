e122f2697ab0bfdc31f8cc8d17afeba7fceed720
b-tice <tice@alum.mit.edu>
2024-01-31 12:32:48 -0800

    multiple saved states

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const Datastore = require('nedb');
 const fs = require('fs');
 const content = '1';
+const content_zero = '0';
 const app = express();
 
+var obj;
+var data_int;
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
@@ -25,13 +28,41 @@ app.post("/api", (request, response) => {
   const data = request.body;
   const timestamp = Date.now();
   
-  fs.writeFile('public/test.txt', content, err => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('File Written OK');// file written successfully
+  fs.readFile('public/test.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log('Data in test.txt:');
+    console.log(data)
+    data_int = parseInt(data, 10); 
+    obj = JSON.parse(data);
+  });
+  
+  if(data_int == 0) {
+    
+    fs.writeFile('public/test.txt', content, err => {
+      if (err) {
+        console.error(err);
+      } 
+      else {
+        console.log('File Written OK: 1');// file written successfully
+      }
+    });    
   }
-});
+  else {
+    
+    if(data_int == 1) {
+    
+    fs.writeFile('public/test.txt', content_zero, err => {
+      if (err) {
+        console.error(err);
+      } 
+      else {
+        console.log('File Written OK: 0');// file written successfully
+      }
+    });    
+  }
+    
+  }
+  
   
   data.timestamp = timestamp;
   database.insert(data);
